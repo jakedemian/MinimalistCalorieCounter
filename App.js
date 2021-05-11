79915b087c94cca6012c9955300c0645be6d1b23
Jake Demian <jakedemian@gmail.com>
2021-05-11 01:33:30 -0400

    the basic v1 of the app is done

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
  * @flow strict-local
  */
 
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -15,6 +15,8 @@ import {
   View,
   Button,
   Image,
+  Modal,
+  TextInput,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
@@ -24,16 +26,19 @@ import AddItem from './src/components/AddItem';
 import ResetDay from './src/components/ResetDay';
 
 const App: () => Node = () => {
+  const inputRef = useRef(null);
   const isDarkMode = useColorScheme() === 'dark';
-  const [dialog, setDialog] = useState({});
   const [calories, setCalories] = useState(null);
   const [dabVisible, setDabVisible] = useState(false);
-
+  const [addItemModalVisible, setAddItemModalVisible] = useState(false);
+  const [resetModalVisible, setResetModalVisible] = useState(false);
+  const [inputValue, setInputValue] = useState('');
   const storeData = async value => {
     try {
       await AsyncStorage.setItem('@Caloreee_calories', String(value));
     } catch (e) {}
   };
+
   const getData = async () => {
     try {
       const jsonValue = await AsyncStorage.getItem('@Caloreee_calories');
@@ -49,37 +54,31 @@ const App: () => Node = () => {
   });
 
   const handleAddItemCancel = () => {
-    setDialog({
-      ...dialog,
-      addItem: false,
-    });
+    setInputValue('');
+    setAddItemModalVisible(false);
   };
-  const handleAddItem = itemCalories => {
-    setDialog({
-      ...dialog,
-      addItem: false,
-    });
+
+  const handleAddItem = () => {
+    const itemCalories = inputValue;
+    if (isNaN(itemCalories)) {
+      return;
+    }
+
+    setInputValue('');
+    setAddItemModalVisible(false);
     const newCals = Number(calories) + Number(itemCalories);
-    console.log(newCals);
     setCalories(newCals);
     storeData(newCals);
+
+    // DAB !!!! \o>
     setDabVisible(true);
     setTimeout(() => {
       setDabVisible(false);
     }, 2000);
   };
 
-  const handleResetDayCancel = () => {
-    setDialog({
-      ...dialog,
-      resetDay: false,
-    });
-  };
   const handleResetDay = () => {
-    setDialog({
-      ...dialog,
-      resetDay: false,
-    });
+    setResetModalVisible(false);
     setCalories(0);
     storeData(0);
   };
@@ -94,27 +93,91 @@ const App: () => Node = () => {
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView contentContainerStyle={backgroundStyle}>
         <View style={styles.container}>
+          {/* ---------------- BEGIN ADD ITEM MODAL ---------------- */}
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={addItemModalVisible}
+            onShow={() => inputRef.current.focus()}>
+            <View style={styles.modal}>
+              <Text style={{fontSize: 30, textAlign: 'center'}}>Add Item</Text>
+              <TextInput
+                ref={inputRef}
+                textAlign="center"
+                style={styles.addItemInput}
+                keyboardType="numeric"
+                placeholder="enter calories"
+                onChangeText={newVal => {
+                  setInputValue(newVal);
+                }}
+                onSubmitEditing={handleAddItem}
+                value={inputValue}
+              />
+              <View style={styles.modalButtons}>
+                <View style={styles.modalButton}>
+                  <Button
+                    onPress={handleAddItemCancel}
+                    color="#f06767"
+                    title="Cancel"
+                  />
+                </View>
+                <View style={styles.modalButton}>
+                  <Button
+                    onPress={handleAddItem}
+                    title="Add"
+                    disabled={!inputValue}
+                  />
+                </View>
+              </View>
+            </View>
+          </Modal>
+          {/* ---------------- END ADD ITEM MODAL ---------------- */}
+
+          {/* ---------------- BEGIN RESET DAY MODAL ---------------- */}
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={resetModalVisible}>
+            <View style={styles.modal}>
+              <Text style={{fontSize: 30, textAlign: 'center'}}>
+                Reset Day?
+              </Text>
+
+              <Text>{inputValue}</Text>
+              <View style={styles.modalButtons}>
+                <View style={styles.modalButton}>
+                  <Button
+                    onPress={() => setResetModalVisible(false)}
+                    color="#f06767"
+                    title="Cancel"
+                  />
+                </View>
+                <View style={styles.modalButton}>
+                  <Button onPress={handleResetDay} title="Reset" />
+                </View>
+              </View>
+            </View>
+          </Modal>
+          {/* ---------------- END RESET DAY MODAL ---------------- */}
+
           <View>
             <Text style={styles.calories}>{calories || 0}</Text>
+            <Text style={{textAlign: 'center'}}>calories today</Text>
           </View>
-          <Button
-            title="Add Item"
-            onPress={() => {
-              setDialog({
-                ...dialog,
-                addItem: true,
-              });
-            }}
-          />
+
+          <View style={styles.spacer}></View>
+
+          <View>
+            <Button
+              onPress={() => setAddItemModalVisible(true)}
+              title="Add Item"
+            />
+          </View>
+
           <View style={styles.resetDay}>
             <Button
+              onPress={() => setResetModalVisible(true)}
               title="Reset Day"
-              onPress={() => {
-                setDialog({
-                  ...dialog,
-                  resetDay: true,
-                });
-              }}
             />
           </View>
           <View
@@ -125,20 +188,6 @@ const App: () => Node = () => {
             }}>
             <Image source={require('./src/assets/fugma_ass.png')} />
           </View>
-
-          {/*Add Food Dialog*/}
-          <AddItem
-            visible={dialog.addItem}
-            cancel={handleAddItemCancel}
-            confirm={handleAddItem}
-          />
-
-          {/*Reset Day Dialog*/}
-          <ResetDay
-            visible={dialog.resetDay}
-            cancel={handleResetDayCancel}
-            confirm={handleResetDay}
-          />
         </View>
       </ScrollView>
     </SafeAreaView>
@@ -154,12 +203,35 @@ const styles = StyleSheet.create({
   },
   calories: {
     fontSize: 85,
-    marginBottom: 64,
+    marginBottom: 0,
+    textAlign: 'center',
   },
   resetDay: {
     position: 'absolute',
     bottom: 16,
     right: 16,
+  },
+  modal: {
+    flex: 1,
+    width: '100%',
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  modalButton: {
+    width: 100,
+    marginRight: 16,
+  },
+  addItemInput: {
+    fontSize: 24,
+  },
+  spacer: {
+    marginTop: 64,
   },
 });
 
